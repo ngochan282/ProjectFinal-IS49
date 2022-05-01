26362d6a1e8a87230fb975c5a73d218ebbd1f643
Trương Ngọc Hân <95341793+ngochan282@users.noreply.github.com>
2022-05-01 18:45:42 +0700

    model.py

---
+++ Model/__init__.py
@@ -0,0 +1,24 @@
+class Account:
+    def __init__(self, username, password):
+        self.username= username
+        self.password= password
+
+class Employee(Account):
+    def __init__(self,username, password, name, id, age, address, role, wage):
+        Account.__init__(self, username, password)
+        self.name= name
+        self.id= id
+        self.age= age
+        self.address= address
+        self.role= role
+        self.wage= wage
+        self.employeelist={}
+
+class Car:
+    def __init__(self,car_id, car_name, quantity, price_in, price_out):
+        self.car_id= car_id
+        self.car_name= car_name
+        self.quantity= quantity
+        self.price_in= price_in
+        self.price_out= price_out
+        self.carlist={}
